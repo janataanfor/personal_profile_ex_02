589cb9124d4ebfd94e53e4848b4521480dd7b47c
oalshokri <janataanfor@gmail.com>
2022-12-05 20:04:04 +0200

    - split material app with new class as stateless widget. - thus we can now use hot reload and invoke build method to reflect the code changes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.purple,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
